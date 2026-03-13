e7ec7ed93659dd7e4b379c101b2525d50064fd23
m-baralt <maria.barranco@upc.edu>
2026-03-13 16:37:34 +0100

    bash script to analyze lit-pcba dataset

---
+++ threshold_experiments/litpcba.sh
@@ -0,0 +1,27 @@
+SCRIPT_DIR="/home/mabarr/TCruzi_pipeline/external/Drug-The-Whole-Genome/"
+cd "$SCRIPT_DIR"
+
+
+echo "First argument: $1"
+
+FOLD_VERSION=6_folds
+use_cache=False
+
+for dir in data/lit_pcba/*/; do
+    target=$(basename "$dir")
+    MOL_PATH="${dir}/mols.lmdb" # "/home/mabarr/TCruzi_pipeline/test/chembl_smiles.lmdb"
+    POCKET_PATH="${dir}/pockets.lmdb"  #"/home/mabarr/TCruzi_pipeline/test/8OZZ.lmdb" 
+    save_path="/home/mabarr/TCruzi_pipeline/threshold_experiments/normalizing_litpcba/${target}.txt"
+
+    CUDA_VISIBLE_DEVICES="1" python ./unimol/run_retrieval.py --user-dir ./unimol $data_path "./dict" --valid-subset test \
+        --num-workers 60 --ddp-backend=c10d --batch-size 4 \
+        --task drugclip-new --loss in_batch_softmax --arch drugclip  \
+        --max-pocket-atoms 511 \
+        --fp16 --fp16-init-scale 4 --fp16-scale-window 256  --seed 1 \
+        --log-interval 100 --log-format simple \
+        --mol-path $MOL_PATH \
+        --pocket-path $POCKET_PATH \
+        --fold-version $FOLD_VERSION \
+        --use-cache $use_cache \
+        --save-path $save_path
+done
